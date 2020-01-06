acb97cc06563372cefc935b99ffb63a7cf4774ee
Saroj Kumar <ssaroj1718@gmail.com>
2020-01-06 10:10:26 +0530

    temp del nodeBlog

--- public/javascripts/main.js
+++
@@ -1,6 +0,0 @@
-function DOM( string ){
-    var {Cc, Ci} = require("chrome");
-    var parser = Cc["@mozilla.org/xmlextras/domparser;1"].createInstance(Ci.nsIDOMParser);
-    console.log("PARSING OF DOM COMPLETED ...");
-    return (parser.parseFromString(string, "text/html"));
-};
